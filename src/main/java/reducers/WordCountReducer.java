1ede8abd2279b2d8f4007fac4ec8d8aa218c6c76
Aaron <aaronvine@gmail.com>
2016-06-03 14:12:49 +0300

    updated reducer

--- src/main/java/reducers/WordCountReducer.java
+++ src/main/java/reducers/WordCountReducer.java
@@ -1,40 +1,17 @@
 package reducers;
 
 import java.io.IOException;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.MapWritable;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class WordCountReducer<KEY> extends Reducer<KEY, MapWritable, KEY, MapWritable> {
+public class WordCountReducer<KEY> extends Reducer<KEY, LongWritable, KEY, LongWritable> {
 
-    private MapWritable mapResult = new MapWritable();
-    private IntWritable decade = new IntWritable();
-    private LongWritable count = new LongWritable();
-
-    public void reduce(KEY key, Iterable<MapWritable> values, Context context)
+    public void reduce(KEY key, Iterable<LongWritable> values, Context context)
             throws IOException, InterruptedException {
-        int i;
-        long currentCount;
-        long countToAdd;
-        long sum;
-        for (i = 1900; i <= 2010; i += 10) {
-            decade.set(i);
-            count.set(0);
-            mapResult.put(decade, count);
+        long sum = 0;
+        for (LongWritable value : values) {
+            sum += value.get();
         }
-        for (MapWritable val : values) {
-            for (i = 1900; i <= 2010; i += 10) {
-                decade.set(i);
-                if (val.containsKey(decade)) {
-                    currentCount = ((LongWritable) mapResult.get(decade)).get();
-                    countToAdd = ((LongWritable) val.get(decade)).get();
-                    sum = currentCount + countToAdd;
-                    count.set(sum);
-                    mapResult.put(decade, count);
-                }
-            }
-        }
-        context.write(key, mapResult);
+        context.write(key, new LongWritable(sum));
     }
 }
