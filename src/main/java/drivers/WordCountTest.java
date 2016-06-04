4f69f02f3763e50f7fd4cb6c26d0c3e795130348
Royvaknin29 <Royvaknin29@gmail.com>
2016-06-04 12:28:27 +0300

    initial push

--- src/main/java/drivers/WordCountTest.java
+++ src/main/java/drivers/WordCountTest.java
@@ -14,31 +14,31 @@ import writable.WordsInDecadeWritable;
 
 public class WordCountTest {
 
-    public static void main(String[] args) throws Exception {
-        System.out.println("Running Word Count Test v8 !");
-        Configuration conf = new Configuration();
-//        conf.set("mapred.map.tasks","10");
-//        conf.set("mapred.reduce.tasks","2");
-        Job job = new Job(conf, "Word Count");
-        System.out.println("Created new job!");
-        job.setJarByClass(WordCountTest.class);
-        System.out.println("JarByClass is set!");
-        job.setMapperClass(WordCountMapper.class);
-        System.out.println("MapperClass is set!");
-        // job.setPartitionerClass(PartitionerClass.class);
-        job.setCombinerClass(WordCountReducer.class);
-        System.out.println("CombinerClass is set!");
-        job.setReducerClass(WordCountReducer.class);
-        System.out.println("ReducerClass is set!");
-        job.setOutputKeyClass(WordsInDecadeWritable.class);
-        System.out.println("OutputKeyClass is set!");
-        job.setOutputValueClass(LongWritable.class);
-        System.out.println("OutputValueClass is set!");
-        job.setInputFormatClass(SequenceFileInputFormat.class);
-        //System.out.println("InputFormatClass is set!");
-        //job.setOutputFormatClass(TextOutputFormat.class);
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
-        System.exit(job.waitForCompletion(true) ? 0 : 1);
-    }
+	public static void main(String[] args) throws Exception {
+		System.out.println("Running Word Count Test v1.0 !");
+		Configuration conf = new Configuration();
+		// conf.set("mapred.map.tasks","10");
+		// conf.set("mapred.reduce.tasks","2");
+		Job job = new Job(conf, "Word Count");
+		System.out.println("Created new job!");
+		job.setJarByClass(WordCountTest.class);
+		System.out.println("JarByClass is set!");
+		job.setMapperClass(WordCountMapper.class);
+		System.out.println("MapperClass is set!");
+		// job.setPartitionerClass(PartitionerClass.class);
+		job.setCombinerClass(WordCountReducer.class);
+		System.out.println("CombinerClass is set!");
+		job.setReducerClass(WordCountReducer.class);
+		System.out.println("ReducerClass is set!");
+		job.setOutputKeyClass(WordsInDecadeWritable.class);
+		System.out.println("OutputKeyClass is set!");
+		job.setOutputValueClass(LongWritable.class);
+		System.out.println("OutputValueClass is set!");
+		job.setInputFormatClass(SequenceFileInputFormat.class);
+		// System.out.println("InputFormatClass is set!");
+		// job.setOutputFormatClass(TextOutputFormat.class);
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
 }
